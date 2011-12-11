80471c0486f489564417208c64d562fff2341804
Josh Adams <josh@isotope11.com>
2011-12-11 16:02:37 -0600

    Add more base functionality to Card
    - require folder name to be within 1..12 and M1..M12
    - add more basic validation tests for Card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,5 @@
 class Card < ActiveRecord::Base
+  VALID_FOLDER_NAMES = (1..31).to_a.map(&:to_s) + ('M1'..'M12').to_a
   validates_presence_of :name, :description, :folder_name
+  validates_inclusion_of :folder_name, in: VALID_FOLDER_NAMES
 end
--- test/unit/card_test.rb
+++ test/unit/card_test.rb
@@ -2,7 +2,27 @@ require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
   def test_is_not_valid_without_name
-    card = Card.new(:description => 'foo', :folder_name => 1)
+    card = Card.new(description: 'foo', folder_name: 1)
     assert !card.valid?
+  end
+
+  def test_is_not_valid_without_description
+    card = Card.new(name: 'foo', folder_name: 1)
+    assert !card.valid?
+  end
+
+  def test_is_not_valid_without_folder_name
+    card = Card.new(name: 'foo', description: 'foo')
+    assert !card.valid?
+  end
+
+  def test_is_not_valid_with_invalid_folder_name
+    card = Card.new(name: 'foo', description: 'foo', folder_name: 'bar')
+    assert !card.valid?
+  end
+
+  def test_is_valid_with_name_description_and_valid_folder_name
+    card = Card.new(name: 'foo', description: 'foo', folder_name: '1')
+    assert card.valid?
   end
 end

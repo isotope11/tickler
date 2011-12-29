a3498b7e689d961acfcaf80195cb75ad1618b010
Josh Adams <josh@isotope11.com>
2011-12-29 10:17:27 -0600

    [Cards] Add cards show

--- test/integration/cards_test.rb
+++ test/integration/cards_test.rb
@@ -1,11 +1,10 @@
 require 'minitest_helper'
 
 describe 'Card integration' do
-  it 'shows me the cards when I go to index' do
-    Factory(:card, :name => 'shave the llama')
-    visit cards_path
-    page.must_have_selector('.cards')
-    within('.cards') do
+  it 'shows me the card when I go to show' do
+    card = Factory(:card, :name => 'shave the llama')
+    visit card_path(card)
+    within('.card') do
       page.must_have_content('shave the llama')
     end
   end
